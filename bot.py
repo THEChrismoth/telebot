5db3953db456c5e10dc1f895d14bd1bdef7145d0
THEChrismoth <30945154+THEChrismoth@users.noreply.github.com>
2022-02-07 16:47:28 +0300

    day1
    Бот консультант
    1)созданы команды start help graphics

--- bot.py
+++ bot.py
@@ -9,8 +9,19 @@ bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def process_start(message: types.Message):
-    await message.reply("Hello, giv me message!")
+async def process_start_command(message: types.Message):
+    await message.reply("Привет, чтобы узнать список команд напиши /help")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("/start-запуск бота и вывод стартового сообщения \n"
+                        "/help-список комманд бота \n"
+                        "/graphics-сыылки на видеокарты Aliexpress")
+
+@dp.message_handler(commands=['gpraphics'])
+async def process_graphics_command(message: types.Message):
+    await message.reply("")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
